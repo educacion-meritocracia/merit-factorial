2d8e59b0f2bfa95e9baf73fc362a6b7773541016
Andreas Lafferte <andreas.lafferte.tamayo@gmail.com>
2025-05-20 11:37:52 -0400

    Update data processing with select vars for paper

--- processing/01-proc-data.R
+++ processing/01-proc-data.R
@@ -39,6 +39,7 @@ db_long <- db_long %>%
   select(id_estudiante,
          ola,
          consent = consentimiento,
+         curse_level = nivel_estudiante,
          perc_effort = p1_1,
          perc_talent = p1_2,
          perc_rich_parents = p1_3,
@@ -46,10 +47,7 @@ db_long <- db_long %>%
          pref_effort = p1_5,
          pref_talent = p1_6,
          pref_rich_parents = p1_7,
-         pref_contact = p1_8,
-         just_educ = p9_3,
-         just_health = p9_4,
-         just_pension = p9_5)
+         pref_contact = p1_8)
 
 # filter ----
 
@@ -57,6 +55,19 @@ db_long <- db_long %>% filter(consent == 1) %>% select(-consent)
 
 # recode and transform ----
 
+# level
+frq(db_long$curse_level)
+
+db_long <- db_long %>% 
+  mutate(
+    cohort_level = if_else(curse_level %in% c("6to", "7mo"), "Primary", "Secondary"),
+    cohort_level = factor(cohort_level, 
+                          labels = c("Primary", "Secondary")),
+    curse_level = factor(curse_level, 
+                         levels = c("6to", "7mo", "1ro", "2do"))
+  )
+
+# merit-scale society
 frq(db_long$perc_effort)
 frq(db_long$perc_talent)
 frq(db_long$perc_rich_parents)
@@ -65,9 +76,7 @@ frq(db_long$pref_effort)
 frq(db_long$pref_talent)
 frq(db_long$pref_rich_parents)
 frq(db_long$pref_contact)
-frq(db_long$just_educ)
-frq(db_long$just_health)
-frq(db_long$just_pension)
+
 
 
 labels1 <- c("Muy en desacuerdo" = 1, 
@@ -78,19 +87,16 @@ labels1 <- c("Muy en desacuerdo" = 1,
              "No responde" = 99)
 
 db_long <- db_long %>% 
-  mutate_at(.vars = (3:13),.funs = ~ sjlabelled::set_labels(., labels = labels1))
+  mutate_at(.vars = (4:11),.funs = ~ sjlabelled::set_labels(., labels = labels1))
 
 
 db_long <- db_long %>% 
   mutate(
     across(
-      .cols = -c(id_estudiante,ola),
+      .cols = -c(id_estudiante,ola,curse_level, cohort_level),
       .fns = ~ set_na(., na = c(88,99))
     )
   )
-
-db_long$mjp <- rowMeans(x = db_long[11:13], na.rm = T)
-db_long$mjp <- if_else(is.nan(db_long$mjp), NA, db_long$mjp)
 
 
 # missings ----
@@ -105,7 +111,7 @@ any_na(db_long)
 
 n_miss(db_long)
 
-prop_miss(db_long[c(3:13)])
+prop_miss(db_long[c(4:11)])
 
 naniar::gg_miss_var(db_long)
 
